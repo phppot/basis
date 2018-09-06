b32eacf6869719ba04c4f63b09832de1d81818a0
Vincy <vincy@phppot.com>
2018-09-06 15:51:28 +0530

    Not required constants removed.

--- Common/Config.php
+++ Common/Config.php
@@ -4,11 +4,7 @@ namespace PhpPot\Common;
 class Config
 {
 
-    const HOME_URL = "http://localhost";
-
     const WORK_ROOT = '/basis/';
 
     const APP_NAME = "basis";
-
-    const DB_FOREIGN_ERRNO = 1451;
 }
